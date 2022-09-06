c06cdc9dc213752a0bc2dafed8265c20870ba405
Bogdan <bodya13371337@gmail.com>
2022-09-06 17:40:53 +0300

    feat:  main view UI added

--- lib/app/router/router_list.dart
+++ lib/app/router/router_list.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:single_house/views/example/example_view.dart';
+import 'package:single_house/views/main/main_view.dart';
 
 abstract class RouterList {
-  static String get defaultRoute => ExampleView.name;
+  static String get defaultRoute => MainView.name;
 
   static Route<dynamic>? onGenerateRoute(RouteSettings args) {
     if (args.name != null && _list[args.name] != null) {
@@ -13,5 +14,6 @@ abstract class RouterList {
 
   static Map<String, PageRoute Function(Object?)?> get _list => {
         ExampleView.name: (arg) => ExampleView.route(),
+        MainView.name: (arg) => MainView.route(),
       };
 }
