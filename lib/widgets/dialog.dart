fa6a5c382f4fbfc4e6e7932d5179d9cf1a7e0fa8
Bogdan <bodya13371337@gmail.com>
2022-09-13 02:07:13 +0300

    feat: added method for show dialog

---
+++ lib/widgets/dialog.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:single_house/app/router/router_core.dart';
+
+void showMaterialDialog({
+  required BuildContext context,
+  required String title,
+  required void Function() callbackYes,
+  void Function()? callbackNo,
+}) {
+  showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: Text(title),
+        actions: <Widget>[
+          TextButton(
+              onPressed: () {
+                if (callbackNo != null) {
+                  callbackNo();
+                }
+                RouterCore.pop();
+              },
+              child: const Text('Ні')),
+          TextButton(
+            onPressed: () {
+              callbackYes();
+            },
+            child: const Text('Так'),
+          )
+        ],
+      );
+    },
+  );
+}
